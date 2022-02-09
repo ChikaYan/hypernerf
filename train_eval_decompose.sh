9c9a9bf205648225a71a09fa54ff0a0b20917f5b
Walter Wu <walterwuyan@gmail.com>
2022-02-09 14:32:22 +0000

    Update configs

--- train_eval_decompose.sh
+++ train_eval_decompose.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 # sbatch command: 
-# sbatch --export=app='./train_eval_decompose.sh' ./slurm_script/run_slurm.sh
+# sbatch --export=application='./train_eval_decompose.sh' ./slurm_script/run_slurm.sh
 
-DATA_PATH='./data/my_hand/'
-LOG_PATH='log/my_hand/decompose_skewed_reg_with_time/'
+# DATA_PATH='./data/my_hand/'
+# LOG_PATH='log/my_hand/skewed_reg/decompose_shadow_loss_re/'
+DATA_PATH='./data/my_hand_large_motion/'
+LOG_PATH='log/my_hand_large_motion/decompose_no_view_dir/'
 
 echo "Log path is: $LOG_PATH"
 
 python train.py --base_folder $LOG_PATH --gin_bindings="data_dir='$DATA_PATH'" --gin_configs configs/decompose/train.gin 
 
 python eval.py --base_folder $LOG_PATH --gin_bindings="data_dir='$DATA_PATH'" --gin_configs configs/decompose/eval.gin
+
 # python eval.py --base_folder $LOG_PATH --gin_bindings="data_dir='$DATA_PATH'" --gin_configs configs/decompose/eval_decompose_static.gin
 # python eval.py --base_folder $LOG_PATH --gin_bindings="data_dir='$DATA_PATH'" --gin_configs configs/decompose/eval_decompose_static_full.gin
 # python eval.py --base_folder $LOG_PATH --gin_bindings="data_dir='$DATA_PATH'" --gin_configs configs/decompose/eval_decompose_dy.gin
--- train_eval_original.sh
+++ train_eval_original.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-DATA_PATH='./data/kubric_car_static_90/'
-LOG_PATH='log/kubric_car_static_90/original_sfm/'
+DATA_PATH='./data/my_hand_large_motion/'
+LOG_PATH='log/my_hand_large_motion/original/'
+
+
+echo "Log path is: $LOG_PATH"
 
 python train.py --base_folder $LOG_PATH --gin_bindings="data_dir='$DATA_PATH'" --gin_configs configs/test_local.gin
 
-python eval.py --base_folder $LOG_PATH --gin_bindings="data_dir='$DATA_PATH'" --gin_configs configs/test_local.gin
+# python eval.py --base_folder $LOG_PATH --gin_bindings="data_dir='$DATA_PATH'" --gin_configs configs/test_local.gin
+python eval.py --base_folder $LOG_PATH --gin_bindings="data_dir='$DATA_PATH'" --gin_configs configs/eval_local.gin
